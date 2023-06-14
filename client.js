52b51bc2e1412bed9ff99c28b98c3a3aa1bfc182
Roman Glazkov <roman.glazkov@altcraft.com>
2023-06-14 13:08:40 +0300

    Add cache-service-worker

---
+++ client.js
@@ -0,0 +1,10 @@
+window.onload = () => navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    window.WebWorkers = registrations;
+
+    const registration = navigator.serviceWorker.register(
+        "cache-service-worker.js",
+        { scope: "/" },
+    );
+
+    console.log('cache-service-worker registration', registration);
+});
--- service-worker.js
+++
@@ -1,1 +0,0 @@
-importScripts('https://pxl.roman-glazkov.dev.altkraft.com/service-worker.js?id=MXwyMg..');
