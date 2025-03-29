e14d820439e90bf0f57ad30d09178180f387d189
Roman Glazkov <glz94@mail.ru>
2025-03-29 19:33:42 +0300

    Replace native buttons with Material UI buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import Button from '@mui/material/Button';
+
 function App() {
-    return null;
+    const handleSubscribe = () => {
+        const value = prompt('Enter an email or user_id for subscription');
+
+        if (value && value.trim().length > 0) {
+            const data = value.includes('@') ? { email: value } : { user_id: value };
+
+            new AKPush().initSubscription(data);
+        }
+    };
+
+    const handleExplicitSubscribe = () => {
+        new AKPush().initSubscription();
+    };
+
+    return (
+        <>
+            <Button
+                variant="contained"
+                size="small"
+                color="primary"
+                onClick={handleSubscribe}
+            >
+                Subscribe to push
+            </Button>
+            <p>If you want to subscribe from Firefox/Safari, please try:</p>
+            <Button
+                variant="contained"
+                size="small"
+                color="primary"
+                onClick={handleExplicitSubscribe}
+            >
+                Subscribe explicitly
+            </Button>
+            <p className="jessy">Element with class .jessy</p>
+        </>
+    );
 }
 
 export default App;
